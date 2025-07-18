5ba6d5d330d7f4e126502af96fbde72e81f80c10
Mutsuki Kano <atamupooh@gmail.com>
2025-07-18 12:51:01 +0900

    Create prac1.py

---
+++ prac1.py
@@ -0,0 +1,14 @@
+# 基本的な文字列の表示
+print(“Hello, World!”)
+
+# 変数の表示
+x = 10
+y = 5
+print(“x:”, x)
+print(“y:”, y)
+print(“Sum of x and y:”, x + y)
+
+# フォーマット文字列を使った出力
+name = “Alice”
+age = 30
+print(f”Name: {name}, Age: {age}”)
